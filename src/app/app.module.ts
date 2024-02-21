505e50a9274d1985e20d715935ceb71d35fad001
Mathieu Charpentier <mathieu.charpentier1993@gmail.com>
2024-02-21 14:58:02 +0100

    Create JWT getter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { JwtModule } from '@auth0/angular-jwt';
+import { HttpClientModule } from "@angular/common/http";
+
+import { environment } from '../environments/environment';
+
+export function tokenGetter() {
+  return localStorage.getItem(`${environment.apiKey}`);
+}
+
 @NgModule({
   declarations: [
     AppComponent
